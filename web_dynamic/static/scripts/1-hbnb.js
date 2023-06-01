21418278c5bd24736fadca08dd8972b491aaaa44
KateMwende <kmwende419@gmail.com>
2023-06-01 16:52:25 +0000

    Made filter section dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  const checkBox = {};
+
+  $('.amenity-checkbox').change(function () {
+    const amenityId = $('this').data('id');
+    const amenityName = $('this').data('name');
+
+    if ($(this).is(':checked')) {
+      checkBox[amenityId] = amenityName;
+    } else {
+      delete checkBox[amenityId];
+    }
+    $('.amenities h4').text(Object.values(checkBox).join(', '));
+  });
+});
