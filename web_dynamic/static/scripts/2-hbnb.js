d9d73f9146424070e72116e035913ba3b8b893af
KateMwende <113633390+KateMwende@users.noreply.github.com>
2023-06-02 17:43:05 +0300

    Update 2-hbnb.js
    Removed quotation around "this"

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   const checkBox = {};
 
   $('.amenity-checkbox').change(function () {
-    const amenityId = $('this').data('id');
-    const amenityName = $('this').data('name');
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
     if ($(this).is(':checked')) {
       checkBox[amenityId] = amenityName;
