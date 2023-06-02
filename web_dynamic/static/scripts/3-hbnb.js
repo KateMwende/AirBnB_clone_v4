639a87562a170dda7f56414314e0827a16f42852
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-06-02 14:16:39 +0300

    Added all the files for task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api-status').addClass('available');
+    } else {
+      $('#api-status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function (data) {
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+          <div class="title">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guests </div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms </div>
+            <div class="number_bathrooms">${place.number_bathrooms}Bathrooms </div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+      }));
+    }
+  });
+
+  const checkBox = {};
+
+  $('.amenity-checkbox').change(function () {
+    const amenityId = $('this').data('id');
+    const amenityName = $('this').data('name');
+
+    if ($(this).is(':checked')) {
+      checkBox[amenityId] = amenityName;
+    } else {
+      delete checkBox[amenityId];
+    }
+    $('.amenities h4').text(Object.values(checkBox).join(', '));
+  });
+});
