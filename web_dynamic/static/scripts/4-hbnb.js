448af7d0dc09cc532f714dccd019e59c412449e9
KateMwende <113633390+KateMwende@users.noreply.github.com>
2023-06-06 16:05:51 +0300

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const checkBox = {};
 
-  $('.amenity-checkbox').change(function () {
+  $('input[type="checkbox"]').change(function () {
     const amenityId = $(this).data('id');
     const amenityName = $(this).data('name');
 
@@ -10,7 +10,7 @@ $(document).ready(function () {
     } else {
       delete checkBox[amenityId];
     }
-    $('.amenities h4').text(Object.values(checkBox).join(', '));
+    $('.amenities > h4').text(Object.values(checkBox).join(', '));
   });
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
