e59068170d812b8c26ac139ad1aa6106f4eab7b4
KateMwende <kmwende419@gmail.com>
2023-06-15 14:36:24 +0000

    Changed port number

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     $('.amenities > h4').text(Object.values(checkBox).join(', '));
   });
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://0.0.0.0:5003/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('#api-status').addClass('available');
     } else {
